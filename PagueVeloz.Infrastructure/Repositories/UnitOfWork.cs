1752c8e5515e9704baeee889c5baef38e60f8ec6
Lucas Montagnani <lucascalilelias@yahoo.com.br>
2025-10-28 01:46:24 -0300

    Adding retry resilience on database transactions

--- PagueVeloz.Infrastructure/Repositories/UnitOfWork.cs
+++ PagueVeloz.Infrastructure/Repositories/UnitOfWork.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
 using PagueVeloz.Domain.Interfaces.Repositories;
 using PagueVeloz.Infrastructure.Persistence.Context;
+using PagueVeloz.Infrastructure.Resiliences;
+using Polly.Retry;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +16,14 @@ namespace PagueVeloz.Infrastructure.Repositories
     {
         private readonly PagueVelozDbContext _context;
         private IDbContextTransaction? _currentTransaction;
+        private readonly ILogger<UnitOfWork> _logger;
+        private readonly AsyncRetryPolicy _retryPolicy;
 
-        public UnitOfWork(PagueVelozDbContext context)
+        public UnitOfWork(PagueVelozDbContext context, ILogger<UnitOfWork> logger)
         {
             _context = context;
+            _logger = logger;
+            _retryPolicy = RetryPolicyFactory.CreateDatabaseRetryPolicy(_logger);
         }
 
         public async Task BeginTransactionAsync()
@@ -24,35 +31,54 @@ namespace PagueVeloz.Infrastructure.Repositories
             if (_currentTransaction != null)
                 return; // já existe uma transação ativa
 
-            _currentTransaction = await _context.Database.BeginTransactionAsync();
+            _currentTransaction = await _retryPolicy.ExecuteAsync(async () =>
+            {
+                _logger.LogInformation("Iniciando transação...");
+                return await _context.Database.BeginTransactionAsync();
+            });
         }
 
         public async Task CommitAsync()
         {
-            if (_currentTransaction == null)
-                throw new InvalidOperationException("Nenhuma transação ativa para commit.");
-
-            await _context.SaveChangesAsync();
-            await _currentTransaction.CommitAsync();
-
-            await _currentTransaction.DisposeAsync();
-            _currentTransaction = null;
+            await _retryPolicy.ExecuteAsync(async () =>
+            {
+                _logger.LogInformation("Tentando commit da transação...");
+                await _context.SaveChangesAsync();
+                if (_currentTransaction != null)
+                {
+                    await _currentTransaction.CommitAsync();
+                    await _currentTransaction.DisposeAsync();
+                    _currentTransaction = null;
+                    _logger.LogInformation("Commit realizado com sucesso.");
+                } 
+                else _logger.LogWarning("Nenhuma transação ativa para realizar o commit.");
+            });
         }
 
         public async Task RollbackAsync()
         {
-            if (_currentTransaction == null)
-                return;
-
-            await _currentTransaction.RollbackAsync();
-            await _currentTransaction.DisposeAsync();
-            _currentTransaction = null;
+            try
+            {
+                if (_currentTransaction != null)
+                {
+                    await _currentTransaction.RollbackAsync();
+                    await _currentTransaction.DisposeAsync();
+                    _currentTransaction = null;
+                    _logger.LogWarning("Rollback executado com sucesso.");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Erro ao executar rollback.");
+            }
         }
 
         public async ValueTask DisposeAsync()
         {
             if (_currentTransaction != null)
                 await _currentTransaction.DisposeAsync();
+
+            await _context.DisposeAsync();
         }
     }
 }
---
+++ PagueVeloz.Infrastructure/Resiliences/RetryPolicyFactory.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using Polly.Retry;
+using Polly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace PagueVeloz.Infrastructure.Resiliences
+{
+    public static class RetryPolicyFactory
+    {
+        public static AsyncRetryPolicy CreateDatabaseRetryPolicy<TLogger>(ILogger<TLogger> logger)
+        {
+            return Policy
+                // Captura exceções comuns em bancos relacionais
+                .Handle<DbUpdateException>()
+                .Or<DbException>()
+                .Or<TimeoutException>()
+                .WaitAndRetryAsync(
+                    retryCount: 3,
+                    sleepDurationProvider: attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)), // 2s, 4s, 8s
+                    onRetry: (exception, timespan, retryCount, context) =>
+                    {
+                        logger.LogWarning(
+                            "Erro ao acessar o banco (tentativa {Retry}) — aguardando {Delay}s. Erro: {Error}",
+                            retryCount,
+                            timespan.TotalSeconds,
+                            exception.Message
+                        );
+                    });
+        }
+    }
+}
